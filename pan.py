1c34f48fc448c94506aa1a5e420fa237ad63d4c9
Redwingshunt <93316521+Redwingshunt@users.noreply.github.com>
2023-06-01 03:02:02 +0530

    initial files
     py and csv data uploaded

---
+++ pan.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+file_path = "D:\\projects\\Py projects\\pandas\\alexandra-compound-health-effects-database\\data\\health_effects.csv"
+
+data_frame = pd.read_csv(file_path)
+
+print(data_frame)
